034a0562187b3105c2559439404c1f6a18694421
Tawsif Ahmed <sif@Tawsifs-MacBook-Pro.local>
2021-12-04 17:30:01 -0500

    Build team section

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import { useState } from "react"
 
 
 export default function Home() {
-  const [carouselImgSelectedNo, setCarouselSelectedImg ] = useState(1)
+  const [ carouselImgSelectedNo, setCarouselSelectedImg ] = useState(1)
+  const [ tabNoSelected, setSelectedTab ] = useState(1)
   return (
     <div className="container">
 
@@ -10,6 +11,8 @@ export default function Home() {
         <title>Tawsif's Coalition Technologies Project</title>
         <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css' />
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Home section */}
         <div className="header-nav top">
             <img 
                 id="header-logo"
@@ -49,7 +52,9 @@ export default function Home() {
             <a className="header-link" href="#team" style={{color: 'black'}}>02. TEAM</a>
           </div>
         </div>
+        {/* Home section */}
 
+        {/* History section */}
         <div id="history">
           <img
             id="history"
@@ -93,9 +98,34 @@ export default function Home() {
               style={carouselImgSelectedNo === 4 ? {border: '2px solid black', opacity: 1.0} : {}}
               onClick={() => setCarouselSelectedImg(4)}
             />
-          </div>
-        
+          </div> 
         </div>
+        {/* History section */}
+
+        {/* Team section */}
+        <div id="team">
+
+          <img 
+            id="team-pre-bg-img"
+            src="/team-pre-bg-img.png"
+          />
+
+          <div className="tab-switcher">
+            <div className={tabNoSelected === 1 ? 'tab tab-selected' : 'tab'} onClick={() => setSelectedTab(1)}>
+              <span>MOUNTAIN 1</span>
+            </div>
+            <div className={tabNoSelected === 2 ? 'tab tab-selected' : 'tab'} onClick={() => setSelectedTab(2)}>
+              <span>MOUNTAIN 2</span>
+            </div>
+          </div>
+
+          <img
+            id="team-bg"
+            src="/bg-pic-3.png"
+          />
+
+        </div>
+        {/* Team section */}
 
         <div>
           {/* <img 
@@ -163,139 +193,36 @@ export default function Home() {
           .carousel-image {
             opacity: 0.4
           }
+          #team {
+            position: absolute;
+            top: 1830px;
+            left: 0px;
+          }
+          #team-pre-bg-img {
+            padding-bottom: 30px;
+            padding-left: 350px;
+          }
+          .tab-switcher {
+            background-color: #41456b;
+            height: 50px;
+            min-height: 50px;
+            display: flex;
+            align-items: center;
+            padding-left: 305px
+          }
+         
+          .tab {
+            display: flex;
+            align-items: center
+            height: 100%;
+            color: white;
+            margin-left: 20px;
+          }
+          .tab-selected {
+            background-color: #b0b4be
+          }
         `}
         </style>
-
-      {/* <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style> */}
 
       <style jsx global>{`
         html,
